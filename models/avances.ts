b6f709afa48320d3fbfb72b11f7a7bd2f59033bf
Cristina Calle <87840049+callecristina@users.noreply.github.com>
2021-12-07 16:35:08 -0500

    modelo de avances

--- models/avances.ts
+++ models/avances.ts
@@ -1,14 +1,40 @@
+import { Schema, model } from "mongoose";
 //Esto es TypeScript
-enum Enum_Rol {
-    estudiant = "Estudiante",
-    lide = "Líder",
-    administrado = "Administrador"
-}
-interface Usuario {
-    nombre: string;
-    apellido: string;
-    correo: string;
-    identificacion: string;
-    rol: Enum_Rol;
+
+interface Avance {
+    fecha: Date;
+    descripcion: string;
+    observaciones: [String];
+    proyecto: Schema.Types.ObjectId;
+    creadopor: Schema.Types.ObjectId;
 }
 //Fin TypeScript
+const avanceSchema = new Schema<Avance>({
+    fecha:{
+        type:Date,
+        required: true,
+        unique: true
+    },
+    descripcion:{
+        type:String,
+        required: true,
+        
+    },
+    observaciones:{
+        type:[String],
+        required: true,
+    },
+    proyecto:{
+        type:Schema.Types.ObjectId,
+        required: true,
+    },
+    creadopor:{
+        type:Schema.Types.ObjectId,
+        required:true
+    }
+
+});
+
+const AvanceModel = model('Avance', avanceSchema);
+
+export { AvanceModel };
